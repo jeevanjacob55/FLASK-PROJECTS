80398f036778e7aaf9b34d86edb84d18380acc3c
jeevanjacob55 <jeevanjacob55@gmail.com>
2025-06-26 13:20:52 +0530

    fixed issues

--- top-secret/main.py
+++ top-secret/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template ,request
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
-
+from flask_bootstrap import Bootstrap5
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired()])
@@ -11,7 +11,7 @@ class LoginForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
-
+bootstrap = Bootstrap5(app) # initialise bootstrap-flask 
 
 @app.route("/")
 def home():
@@ -21,17 +21,12 @@ def home():
 @app.route("/login",methods = ["GET","POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
-    if request.method == 'GET':
-        return render_template('login.html', form=login_form)
-    if login_form.email.data == "admin@gmail.com" and login_form.password.data == "12345678":
-        return render_template("success.html")
-    else:
-        return render_template("denied.html")
-
-
-        
-
-
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@gmail.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=login_form)
+    
 if __name__ == '__main__':
     app.run(debug=True)
